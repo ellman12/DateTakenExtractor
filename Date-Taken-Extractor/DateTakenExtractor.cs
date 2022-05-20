88aa34ad6bd48b6398188e40eb45284594ebaf53
Elliott <ellduc4@gmail.com>
2022-05-20 17:51:09 -0500

    Publish first NuGet package: this, but literally nothing in here lmao

---
+++ Date-Taken-Extractor/DateTakenExtractor.cs
@@ -0,0 +1,11 @@
+﻿namespace Date_Taken_Extractor
+{
+    public class DateTakenExtractor
+    {
+        public void Test()
+        {
+            Console.WriteLine("hi");
+            Console.WriteLine("hello");
+        }
+    }
+}
