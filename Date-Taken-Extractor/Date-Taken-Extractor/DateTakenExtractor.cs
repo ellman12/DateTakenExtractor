943fbeeefc1870ef7ce151448c4459724ffedfcf
Elliott <ellduc4@gmail.com>
2022-05-20 23:32:07 -0500

    Start laying groundwork of the class

--- Date-Taken-Extractor/Date-Taken-Extractor/DateTakenExtractor.cs
+++ Date-Taken-Extractor/Date-Taken-Extractor/DateTakenExtractor.cs
@@ -1,30 +1,69 @@
-﻿using System.Globalization;
-using ExifLib;
-using MetadataExtractor;
-using MetadataExtractor.Formats.Exif;
-using Directory = MetadataExtractor.Directory;
-using ExifReader = MetadataExtractor.Formats.Exif.ExifReader;
+﻿namespace Date_Taken_Extractor;
 
-namespace Date_Taken_Extractor;
-
+///Contains static methods for making it easy to get Date Taken metadata from photos and videos.
 public static class DateTakenExtractor
 {
-	private const string TEST_PATH = "C:/Users/Elliott/Videos/Photos-001/IMG_20210426_145048.jpg";
-	
-	public static string MD()
+	///Used to represent where a Date Taken value came from in a file.
+	public enum DateTakenSrc
 	{
-		IEnumerable<Directory> directories = ImageMetadataReader.ReadMetadata(TEST_PATH);
-		ExifSubIfdDirectory? subIfdDirectory = directories.OfType<ExifSubIfdDirectory>().FirstOrDefault();
-		string? dateTime = subIfdDirectory!.GetDescription(ExifDirectoryBase.TagDateTime);
-		if (dateTime == null)
-			return "null";
-		return dateTime;
+		///Date Taken came from file's metadata.
+		Metadata, 
+		
+		///Date Taken came from filename.
+		Filename,
+		
+		///No Date Taken in metadata or filename.
+		None
+	}
+	
+	///<summary>First tries to find Date Taken in the metadata of the file. If it can, uses that. If it can't, looks in the filename. If no data in both, return null.</summary>
+	/// <param name="fullPath">The full path to the file.</param>
+	/// <param name="dateTaken">The DateTime? variable to store the Date Taken in.</param>
+	///<exception cref="ArgumentNullException">Thrown if fullPath is null.</exception>
+	///<exception cref="ArgumentException">Thrown if fullPath is a file that doesn't exist.</exception>
+	/// <returns>DateTakenSrc enum representing where the Date Taken came from.</returns>
+	public static DateTakenSrc GetDateTimeAuto(string fullPath, out DateTime? dateTaken)
+	{
+		if (fullPath == null) throw new ArgumentNullException(nameof(fullPath));
+		if (!File.Exists(fullPath)) throw new ArgumentException("File specified does not exist.");
+	}
+	
+	///<summary>Date Taken metadata from just the file's internal metadata.</summary>
+	///<param name="fullPath">The full path to the file.</param>
+	///<exception cref="ArgumentNullException">Thrown if fullPath is null.</exception>
+	///<exception cref="ArgumentException">Thrown if fullPath is a file that doesn't exist.</exception>
+	///<returns>The Date Taken that was found in the metadata, otherwise null.</returns>
+	public static DateTime? GetDateTakenFromMetadata(string fullPath)
+	{
+		if (fullPath == null) throw new ArgumentNullException(nameof(fullPath));
+		if (!File.Exists(fullPath)) throw new ArgumentException("File specified does not exist.");
 	}
 
-	public static string Exif()
+	///<summary>Get Date Taken metadata from just the filename.</summary>
+	///<param name="fullPath">The full path to the file.</param>
+	///<exception cref="ArgumentNullException">Thrown if fullPath is null.</exception>
+	///<exception cref="ArgumentException">Thrown if fullPath is a file that doesn't exist.</exception>
+	///<returns>The Date Taken that was found in the filename, otherwise null.</returns>
+	public static DateTime? GetDateTakenFromFilename(string fullPath)
 	{
-		ExifLib.ExifReader reader = new(TEST_PATH);
-		reader.GetTagValue(ExifTags.DateTimeDigitized, out DateTime dateTaken);
-		return dateTaken.ToString(CultureInfo.InvariantCulture);
+		if (fullPath == null) throw new ArgumentNullException(nameof(fullPath));
+		if (!File.Exists(fullPath)) throw new ArgumentException("File specified does not exist.");
+	}
+	
+	///<summary>Get Date Taken from metadata AND the filename.</summary>
+	///<param name="fullPath">The full path to the file.</param>
+	///<param name="metadataDT">The DateTime? variable to store the metadata Date Taken in.</param>
+	///<param name="filenameDT">The DateTime? variable to store the filename Date Taken in.</param>
+	///<exception cref="ArgumentNullException">Thrown if fullPath is null.</exception>
+	///<exception cref="ArgumentException">Thrown if fullPath is a file that doesn't exist.</exception>
+	public static void GetDateTakenFromBoth(string fullPath, out DateTime? metadataDT, out DateTime? filenameDT)
+	{
+		if (fullPath == null) throw new ArgumentNullException(nameof(fullPath));
+		if (!File.Exists(fullPath)) throw new ArgumentException("File specified does not exist.");
+	}
+
+	private static DateTime? GetFilenameDT(string filename)
+	{
+		//TODO: try regex here lmao
 	}
 }
