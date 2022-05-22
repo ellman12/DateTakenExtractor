b73f14089b638430ad166eaff7dd1ea6881670c1
Elliott <ellduc4@gmail.com>
2022-05-22 11:40:04 -0500

    Move filename method down farther

--- Date-Taken-Extractor/Date-Taken-Extractor/DateTakenExtractor.cs
+++ Date-Taken-Extractor/Date-Taken-Extractor/DateTakenExtractor.cs
@@ -1,6 +1,7 @@
 using System.Text.RegularExpressions;
 using MetadataExtractor;
 using MetadataExtractor.Formats.Exif;
+using MetadataExtractor.Formats.Png;
 using MetadataExtractor.Formats.QuickTime;
 
 namespace Date_Taken_Extractor;
@@ -69,24 +70,6 @@ public static class DateTakenExtractor
 	}
 	*/
 
-	///<summary>Analyzes a filename to see if it has a timestamp in it.</summary>
-	///<param name="filename">The filename to analyze, with or without the file extension.</param>
-	///<returns>A DateTime? representing the timestamp that was found in the file. null if couldn't find a timestamp.</returns>
-	public static DateTime? AnalyzeFilename(string filename)
-	{
-		//Each thing in () is considered a group. First part is for the junk data. Adds it to a group so it stays away from the other useful groups. I think it only comes from Steam screenshots.
-		//The '[-_\. ]?' handle the presence or absence of separator characters (' ', '-', '_', '.') present in most filenames, like 'IMG_20210320_175909.jpg', 'Capture 2020-12-26 21_03_05.png', and '2020-10-06_13.53.33.png'.
-		const string PATTERN = @"(\d+[-_ ])?(\d{4})[-_\. ]?(\d{2})[-_\. ]?(\d{2})[-_\. ]?(\d{2})[-_\. ]?(\d{2})[-_\. ]?(\d{2})";
-		MatchCollection matches = new Regex(PATTERN).Matches(filename);
-		if (matches.Count == 0) return null; //.Count should only ever be 0 or 1 with this pattern, since there should only ever be 0 or 1 matches.
-		
-		//groups[0] is the whole match that was returned, which may or may not contain extra junk data that isn't used, like extension, and other characters.
-		//groups[1] could contain some extra junk at the start of the filename, (e.g., 105600 from Steam screenshots), so it's ignored.
-		//groups[2] is the year, groups[3] is the month, etc.
-		GroupCollection groups = matches[0].Groups;
-		return DateTime.Parse($"{groups[2]}-{groups[3]}-{groups[4]} {groups[5]}:{groups[6]}:{groups[7]}");
-	}
-	
 	///<summary>Analyzes the Exif metadata (if any) of a (usually image) file.</summary>
 	///<param name="fullPath">Full path to the item to analyze.</param>
 	///<returns>A DateTime? representing the Date Taken metadata that was found in the file. null if couldn't find any data.</returns>
@@ -131,6 +114,24 @@ public static class DateTakenExtractor
 		{
 			return null;
 		}
+	}
+	
+	///<summary>Analyzes a filename to see if it has a timestamp in it.</summary>
+	///<param name="filename">The filename to analyze, with or without the file extension.</param>
+	///<returns>A DateTime? representing the timestamp that was found in the file. null if couldn't find a timestamp.</returns>
+	public static DateTime? AnalyzeFilename(string filename)
+	{
+		//Each thing in () is considered a group. First part is for the junk data. Adds it to a group so it stays away from the other useful groups. I think it only comes from Steam screenshots.
+		//The '[-_\. ]?' handle the presence or absence of separator characters (' ', '-', '_', '.') present in most filenames, like 'IMG_20210320_175909.jpg', 'Capture 2020-12-26 21_03_05.png', and '2020-10-06_13.53.33.png'.
+		const string PATTERN = @"(\d+[-_ ])?(\d{4})[-_\. ]?(\d{2})[-_\. ]?(\d{2})[-_\. ]?(\d{2})[-_\. ]?(\d{2})[-_\. ]?(\d{2})";
+		MatchCollection matches = new Regex(PATTERN).Matches(filename);
+		if (matches.Count == 0) return null; //.Count should only ever be 0 or 1 with this pattern, since there should only ever be 0 or 1 matches.
+		
+		//groups[0] is the whole match that was returned, which may or may not contain extra junk data that isn't used, like extension, and other characters.
+		//groups[1] could contain some extra junk at the start of the filename, (e.g., 105600 from Steam screenshots), so it's ignored.
+		//groups[2] is the year, groups[3] is the month, etc.
+		GroupCollection groups = matches[0].Groups;
+		return DateTime.Parse($"{groups[2]}-{groups[3]}-{groups[4]} {groups[5]}:{groups[6]}:{groups[7]}");
 	}
 
 	///<summary>Take a timestamp string like '2018-11-03 07:26:12', or of similar format, and attempt to parse and return a DateTime representing it.</summary>
