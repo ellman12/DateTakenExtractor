b44d4ee81087dc70dde52e8a455410cd3d11b3a8
Elliott <ellduc4@gmail.com>
2022-05-22 21:24:13 -0500

    finalize some stuff

--- DateTakenExtractor/DateTakenExtractor.cs
+++ DateTakenExtractor/DateTakenExtractor.cs
@@ -3,7 +3,7 @@ using MetadataExtractor;
 using MetadataExtractor.Formats.Exif;
 using MetadataExtractor.Formats.QuickTime;
 
-namespace Date_Taken_Extractor;
+namespace DateTakenExtractor;
 
 ///Contains static methods for making it easy to get Date Taken metadata from photos and videos.
 public static class DateTakenExtractor
