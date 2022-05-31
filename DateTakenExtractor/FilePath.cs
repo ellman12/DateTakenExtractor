2c56f410a95ff6375a373bfcde1396c8f5b727da
Elliott <ellduc4@gmail.com>
2022-05-30 21:47:50 -0500

    Start laying groundwork for adding overloads that use FileStreams. Reorganize, etc.

--- DateTakenExtractor/FilePath.cs
+++ DateTakenExtractor/FilePath.cs
@@ -1,4 +1,3 @@
-using System.Text.RegularExpressions;
 using MetadataExtractor;
 using MetadataExtractor.Formats.Exif;
 using MetadataExtractor.Formats.QuickTime;
@@ -6,21 +5,9 @@ using MetadataExtractor.Formats.QuickTime;
 namespace DateTakenExtractor;
 
 ///Contains static methods for making it easy to get Date Taken metadata from photos and videos.
-public static class DateTakenExtractor
+//This file contains methods that accept direct file paths as arguments.
+public static partial class DateTakenExtractor
 {
-	///Used to represent where a Date Taken value came from in a file.
-	public enum DateTakenSrc
-	{
-		///Date Taken came from file's metadata.
-		Metadata, 
-		
-		///Date Taken came from filename.
-		Filename,
-		
-		///No Date Taken in metadata or filename.
-		None
-	}
-
 	///<summary>First tries to find Date Taken in the metadata of the file. If it can, uses that. If it can't find a DT in the metadata, looks in the filename. If no data in both, return null.</summary>
 	///<param name="fullPath">The full path to the file.</param>
 	///<param name="dateTakenSrc">'Metadata' if found DT in metadata. 'Filename' if DT came from filename. 'None' if no DT found and thus DT is null.</param>
@@ -67,20 +54,6 @@ public static class DateTakenExtractor
 		return dateTaken; //← Could be null or an actual value from this ↑.
 	}
 
-	///<summary>Get Date Taken metadata from just the filename.</summary>
-	///<param name="filename">The filename to analyze. You <i>can</i> also give it the full path to the file and it <i>might</i> work, but passing in just the filename is preferred.</param>
-	///<exception cref="ArgumentNullException">Thrown if filename is null.</exception>
-	///<returns>A DateTime? representing the Date Taken that was found in the filename, otherwise null.</returns>
-	///<remarks>If you pass in a full path instead of a filename, it will attempt to strip out the extra characters and get just the filename, which is then used.</remarks>
-	public static DateTime? GetDateTakenFromFilename(string filename)
-	{
-		if (filename == null) throw new ArgumentNullException(nameof(filename));
-		if (Path.IsPathFullyQualified(filename)) Path.GetFileNameWithoutExtension(filename);
-		
-		DateTime? dateTaken = AnalyzeFilename(filename);
-		return dateTaken;
-	}
-	
 	///<summary>Get Date Taken from both metadata AND the filename, when possible.</summary>
 	///<param name="fullPath">The full path to the file.</param>
 	///<param name="metadataDT">The DateTime? variable to store the metadata Date Taken in.</param>
@@ -137,47 +110,6 @@ public static class DateTakenExtractor
 			return null; //No DT metadata in file.
 		}
 		catch (Exception e) when (e is UnauthorizedAccessException or InvalidOperationException) //In testing, UnauthorizedAccessExceptions only happened for Switch clips. InvalidOperationExceptions can happen when no metadata in file.
-		{
-			return null;
-		}
-	}
-	
-	///<summary>Analyzes a filename to see if it has a timestamp in it.</summary>
-	///<param name="filename">The filename to analyze, with or without the file extension.</param>
-	///<returns>A DateTime? representing the timestamp that was found in the file. null if couldn't find a timestamp.</returns>
-	private static DateTime? AnalyzeFilename(string filename)
-	{
-		//Each thing in () is considered a group. First part is for the junk data. Adds it to a group so it stays away from the other useful groups. I think it only comes from Steam screenshots.
-		//The '[-_\. ]?' handle the presence or absence of separator characters (' ', '-', '_', '.') present in most filenames, like 'IMG_20210320_175909.jpg', 'Capture 2020-12-26 21_03_05.png', and '2020-10-06_13.53.33.png'.
-		const string PATTERN = @"(\d+[-_\.: ])?(\d{4})[-_\.: ]?(\d{2})[-_\.: ]?(\d{2})[-_\.: ]?(\d{2})[-_\.: ]?(\d{2})[-_\.: ]?(\d{2})";
-		MatchCollection matches = new Regex(PATTERN).Matches(filename);
-		if (matches.Count == 0) return null; //.Count should only ever be 0 or 1 with this pattern, since there should only ever be 0 or 1 matches.
-
-		//groups[0] is the whole match that was returned, which may or may not contain extra junk data that isn't used, like extension, and other characters.
-		//groups[1] could contain some extra junk at the start of the filename, (e.g., 105600 from Steam screenshots), so it's ignored.
-		//groups[2] is the year, groups[3] is the month, etc.
-		GroupCollection groups = matches[0].Groups;
-		return DateTime.Parse($"{groups[2]}-{groups[3]}-{groups[4]} {groups[5]}:{groups[6]}:{groups[7]}");
-	}
-
-	///<summary>Take a timestamp string like '2018-11-03 07:26:12', or of similar format, and attempt to parse and return a DateTime representing it.</summary>
-	///<param name="timestamp">The timestamp to attempt to parse.</param>
-	///<returns>A DateTime? representing the parsed timestamp. null if couldn't determine Date Taken.</returns>
-	private static DateTime? Parse(string timestamp)
-	{
-		try
-		{
-			//Remove any useless characters, and turn all the parts of the timestamp into integers for creating the DateTime.
-			timestamp = timestamp.Replace(" ", "").Replace("-", "").Replace("_", "").Replace(".", "").Replace(":", "").Replace(";", "");
-			int year = Int32.Parse(timestamp[..4]);
-			int month = Int32.Parse(timestamp[4..6]);
-			int day = Int32.Parse(timestamp[6..8]);
-			int hour = Int32.Parse(timestamp[8..10]);
-			int min = Int32.Parse(timestamp[10..12]);
-			int sec = Int32.Parse(timestamp[12..14]);
-			return new DateTime(year, month, day, hour, min, sec);
-		}
-		catch (FormatException)
 		{
 			return null;
 		}
---
+++ DateTakenExtractor/FileStream.cs
@@ -0,0 +1,8 @@
+namespace DateTakenExtractor;
+
+///Contains static methods for making it easy to get Date Taken metadata from photos and videos.
+//This file contains methods that accept FileStreams as arguments.
+public static partial class DateTakenExtractor
+{
+	
+}
