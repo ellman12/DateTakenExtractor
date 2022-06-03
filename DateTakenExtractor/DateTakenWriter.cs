a1eec777d9355e50a980135d6bce749883ced8c1
Elliott <ellduc4@gmail.com>
2022-06-03 16:03:07 -0500

    Bunch of comments and implemented (but not tested) the 2 public methods

--- DateTakenExtractor/DateTakenWriter.cs
+++ DateTakenExtractor/DateTakenWriter.cs
@@ -5,20 +5,30 @@ namespace DateTakenExtractor;
 //Contains static methods for writing/modifying Date Taken metadata in photos and videos.
 public static partial class DateTakenExtractor
 {
-	///<summary></summary>
-	///<param name="fullPath"></param>
-	public static void UpdateDateTaken(string fullPath)
+	///<summary>Uses ExifTool to update the internal Date Taken metadata of the specified photo or video.</summary>
+	///<param name="fullPath">The full path to the item to update</param>
+	///<param name="newDateTaken">The new Date Taken to embed in the file's metadata.</param>
+	public static void UpdateDateTaken(string fullPath, DateTime newDateTaken)
 	{
-		
-	}
-
-	
-	public static void UpdateDateTaken(FileStream fileStream)
-	{
-		
+		using FileStream fileStream = new(fullPath, FileMode.Open, FileAccess.ReadWrite, FileShare.None); //TODO: this .None option might cause problems...
+		string ext = Path.GetExtension(fileStream.Name).ToLower();
+		if (IsPhotoExt(ext)) UpdatePhotoDateTaken(fileStream.Name, newDateTaken);
+		else if (IsVideoExt(ext)) UpdateVideoDateTaken(fileStream.Name, newDateTaken);
 	}
 	
-	private static void UpdateImageDateTaken(string fullPath, DateTime newDateTaken)
+	///<summary>Uses ExifTool to update the internal Date Taken metadata of the specified photo or video.</summary>
+	/// <param name="fileStream">FileStream of the item to update.</param>
+	///<param name="newDateTaken">The new Date Taken to embed in the file's metadata.</param>
+	public static void UpdateDateTaken(FileStream fileStream, DateTime newDateTaken)
+	{
+		string ext = Path.GetExtension(fileStream.Name).ToLower();
+		if (IsPhotoExt(ext)) UpdatePhotoDateTaken(fileStream.Name, newDateTaken);
+		else if (IsVideoExt(ext)) UpdateVideoDateTaken(fileStream.Name, newDateTaken);
+	}
+	
+	///<summary>Uses ExifTool to update the internal Date Taken metadata of the specified photo.</summary>
+	///<param name="fullPath">The full path to the item to update.</param>
+	///<param name="newDateTaken">The new Date Taken to embed in the file's metadata.</param>
 	private static void UpdatePhotoDateTaken(string fullPath, DateTime newDateTaken)
 	{
 		string DT = newDateTaken.ToString("yyyy:M:d H:mm:ss");
@@ -41,6 +51,9 @@ public static partial class DateTakenExtractor
 		process.Start();
 	}
 
+	///<summary>Uses ExifTool to update the internal Date Taken metadata of the specified video.</summary>
+	///<param name="fullPath">The full path to the item to update.</param>
+	///<param name="newDateTaken">The new Date Taken to embed in the file's metadata.</param>
 	private static void UpdateVideoDateTaken(string fullPath, DateTime newDateTaken)
 	{
 		string DT = newDateTaken.ToString("yyyy:M:d H:mm:ss");
