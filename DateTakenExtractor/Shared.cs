ed9a98fa4153e5705d7e15faff331eb64a3f10d0
Elliott <ellduc4@gmail.com>
2022-06-04 16:57:24 -0500

    IsPhotoExt and IsVideoExt now throw ArgumentException if unsupported ext

--- DateTakenExtractor/Shared.cs
+++ DateTakenExtractor/Shared.cs
@@ -21,13 +21,23 @@ public static partial class DateTakenExtractor
 
 	///<summary>Determine if the specified extension is a photo extension. The supported extensions are ".jpg", ".jpeg", ".png", and ".gif".</summary>
 	///<param name="ext">The extension to analyze.</param>
+	///<exception cref="ArgumentException">Thrown if ext is not a supported extension.</exception>
 	///<returns>True if the extension was an photo extension, false otherwise.</returns>
-	private static bool IsPhotoExt(string ext) => ext.ToLower() is ".jpg" or ".jpeg" or ".png" or ".gif";
-	
+	private static bool IsPhotoExt(string ext)
+	{
+		if (ext.ToLower() is ".jpg" or ".jpeg" or ".png" or ".gif") return true;
+		throw new ArgumentException($"Unsupported file extension: {ext}");
+	}
+
 	///<summary>Determine if the specified extension is a video extension. The supported extensions are ".mp4", ".mov", and ".mkv".</summary>
 	///<param name="ext">The extension to analyze.</param>
+	///<exception cref="ArgumentException">Thrown if ext is not a supported extension.</exception>
 	///<returns>True if the extension was a video extension, false otherwise.</returns>
-	private static bool IsVideoExt(string ext) => ext.ToLower() is ".mp4" or ".mov" or ".mkv";
+	private static bool IsVideoExt(string ext)
+	{
+		if (ext.ToLower() is ".mp4" or ".mov" or ".mkv") return true;
+		throw new ArgumentException($"Unsupported file extension: {ext}");
+	}
 
 	///<summary>Get Date Taken metadata from just the filename.</summary>
 	///<param name="filename">The filename to analyze. You <i>can</i> also give it the full path to the file and it <i>might</i> work, but passing in just the filename is preferred.</param>
