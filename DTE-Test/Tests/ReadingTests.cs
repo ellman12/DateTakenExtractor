65e167c9d8a7f50645488b2fa33cb4b42cc7084d
Elliott <ellduc4@gmail.com>
2023-07-11 15:26:30 -0500

    Create first reading unit test, ReadBothDTTypesForAllFilesTest

--- DTE-Test/Tests/ReadingTests.cs
+++ DTE-Test/Tests/ReadingTests.cs
@@ -3,5 +3,18 @@
 [TestFixture]
 public class ReadingTests
 {
-	
+	[Test]
+	public void ReadBothDTTypesForAllFiles()
+	{
+		foreach (TF tf in TF.TestFiles)
+		{
+			string path = Path.Join(TF.TestFilesPath, tf.Filename);
+			D.GetDateTakenFromBoth(path, out DateTime? metadataDT, out DateTime? filenameDT);
+			
+			if (tf.MetadataDT != metadataDT)
+				Assert.Fail($"{tf.Filename}'s metadata DT of {tf.MetadataDT} does not match the expected value of {metadataDT}");
+			else if (tf.FilenameDT != filenameDT)
+				Assert.Fail($"{tf.Filename}'s filename DT of {tf.FilenameDT} does not match the expected value of {filenameDT}");
+		}
+	}
 }
