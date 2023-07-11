a25c07382260ace10c99d6c1f30708d71ed30bed
Elliott <ellduc4@gmail.com>
2023-07-11 14:19:52 -0500

    TestFile record can now read and (if needed) update the JSON file

--- DTE-Test/TestFile.cs
+++ DTE-Test/TestFile.cs
@@ -1,3 +1,33 @@
-﻿namespace DTE_Test;
+﻿using System.Text.Json;
 
-public record TestFile (string Filename, DateTime? MetadataDT, DateTime? FilenameDT);
+namespace DTE_Test;
+
+public record TestFile(string Filename, DateTime? MetadataDT, DateTime? FilenameDT)
+{
+	public static readonly List<TestFile> TestFiles;
+
+	public static string TestFilesPath => Path.Combine(Environment.CurrentDirectory.Replace("\\", "/").Replace("bin/Debug/net7.0", null), "Test Files");
+	
+	public static string JsonPath => Path.Combine(TestFilesPath, "/TestFiles.json");
+
+	static TestFile()
+	{
+		string json = File.ReadAllText(JsonPath);
+		TestFiles = JsonSerializer.Deserialize<List<TestFile>>(json)!;
+	}
+
+	///(Re-)Initializes the JSON file containing data about each file. Only needs to be ran when modifying the 'Test Files' folder.
+	private static void JsonInitialization()
+	{
+		List<TestFile> items = new();
+	
+		foreach (string file in Directory.GetFiles(TestFilesPath))
+		{
+			string filename = Path.GetFileName(file);
+			TestFile newItem = new(filename, D.GetDateTakenFromMetadata(file), D.GetDateTakenFromFilename(filename));
+			items.Add(newItem);
+		}
+	
+		File.WriteAllText(JsonPath, JsonSerializer.Serialize(items));
+	}
+}
