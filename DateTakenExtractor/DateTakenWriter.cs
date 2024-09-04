2740205fa9b33cf2aa556015f86aadd3bf48fe4f
Elliott DuCharme <ellduc4@gmail.com>
2024-09-04 12:47:54 -0500

    DateTakenWriter now modifies filesystem access/modify/create date metadata

--- DateTakenExtractor/DateTakenWriter.cs
+++ DateTakenExtractor/DateTakenWriter.cs
@@ -14,14 +14,14 @@ public static partial class DateTakenExtractor
 		if (IsPhotoExt(ext)) UpdatePhotoDateTaken(fullPath, newDateTaken);
 		else if (IsVideoExt(ext)) UpdateVideoDateTaken(fullPath, newDateTaken);
 	}
-	
+
 	///<summary>Uses ExifTool to update the internal Date Taken metadata of the specified photo.</summary>
 	///<param name="fullPath">The full path to the item to update.</param>
 	///<param name="newDateTaken">The new Date Taken to embed in the file's metadata. If null, removes any Date Taken metadata already present.</param>
 	private static void UpdatePhotoDateTaken(string fullPath, DateTime? newDateTaken)
 	{
-		string? DT = newDateTaken?.ToString("yyyy:M:d H:mm:ss");
-		
+		string DT = FormatDateTaken(newDateTaken);
+
 		using Process process = new()
 		{
 			StartInfo = new ProcessStartInfo
@@ -29,7 +29,7 @@ public static partial class DateTakenExtractor
 				//https://exiftool.org/TagNames/EXIF.html
 				//https://exiftool.org/TagNames/Shortcuts.html
 				FileName = "exiftool",
-				Arguments = $"\"{fullPath}\" -overwrite_original -AllDates=\"{DT}\" {(Path.GetExtension(fullPath).ToLower() == ".png" ? $"-PNG:CreationTime=\"{DT}\" -PNG:ModifyDate=\"{DT}\"" : "")}",
+				Arguments = $"\"{fullPath}\" -overwrite_original -AllDates={DT} -FileAccessDate={DT} -FileCreateDate={DT} -FileModifyDate={DT} {(Path.GetExtension(fullPath).ToLower() == ".png" ? $"-PNG:CreationTime={DT} -PNG:ModifyDate={DT}" : "")}",
 				CreateNoWindow = true,
 				RedirectStandardError = false,
 				RedirectStandardInput = false,
@@ -47,8 +47,8 @@ public static partial class DateTakenExtractor
 	private static void UpdateVideoDateTaken(string fullPath, DateTime? newDateTaken)
 	{
 		//Videos require DateTime to be in UTC for some reason: https://exiftool.org/forum/index.php?PHPSESSID=a68f2cbabc087b534d7ac88e55fb932d&topic=11880.msg64084#msg64084
-		string? DT = newDateTaken?.ToString("yyyy:M:d H:mm:ss");
-		
+		string DT = FormatDateTaken(newDateTaken);
+
 		using Process process = new()
 		{
 			StartInfo = new ProcessStartInfo
@@ -56,7 +56,7 @@ public static partial class DateTakenExtractor
 				//https://exiftool.org/forum/index.php?topic=11100.msg59329#msg59329
 				//https://exiftool.org/forum/index.php?topic=11272
 				FileName = "exiftool",
-				Arguments = $"\"{fullPath}\" -overwrite_original -CreateDate=\"{DT}\" -ModifyDate=\"{DT}\" -Track*Date=\"{DT}\" -Media*Date=\"{DT}\" -Quicktime:DateTimeOriginal=\"{DT}\"",
+				Arguments = $"\"{fullPath}\" -overwrite_original -CreateDate={DT} -ModifyDate={DT} -Track*Date={DT} -Media*Date={DT} -Quicktime:DateTimeOriginal={DT} -FileAccessDate={DT} -FileCreateDate={DT} -FileModifyDate={DT}",
 				CreateNoWindow = true,
 				RedirectStandardError = false,
 				RedirectStandardInput = false,
@@ -66,5 +66,11 @@ public static partial class DateTakenExtractor
 		};
 		process.Start();
 		process.WaitForExit();
+	}
+
+	private static string FormatDateTaken(DateTime? dateTaken)
+	{
+		string? DT = dateTaken?.ToString("yyyy:M:d H:mm:ss");
+		return DT == null ? "" : $"\"{DT}\"";
 	}
 }
